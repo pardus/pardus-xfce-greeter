95aaa8e0b3cb8139ad2cff5aab0684b562334e54
Emin Fedar <eminfedar@gmail.com>
2021-06-01 08:05:46 -0400

    setup fixes

--- setup.py
+++ setup.py
@@ -17,11 +17,10 @@ data_files = [
     ("/usr/share/applications/", ["tr.org.pardus.welcome.desktop"]),
     ("/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/LC_MESSAGES/pardus-welcome.mo"]),
     ("/usr/share/pardus/pardus-welcome/", ["icon.svg", "pardus-logo.svg", "theme-light.png", "theme-dark.png"]),
-    ("/usr/share/pardus/pardus-welcome/src",
-     ["src/main.py", "src/MainWindow.py", "src/utils.py",
-        "src/xfce/WallpaperManager.py", "src/xfce/ThemeManager.py", "src/xfce/ScaleManager.py",
-        "src/gnome/WallpaperManager.py", "src/gnome/ThemeManager.py", "src/gnome/ScaleManager.py"]),
-    ("/usr/share/pardus/pardus-image-writer/ui", ["ui/MainWindow.glade"]),
+    ("/usr/share/pardus/pardus-welcome/src", ["src/main.py", "src/MainWindow.py", "src/utils.py"]),
+    ("/usr/share/pardus/pardus-welcome/src/xfce", ["src/xfce/WallpaperManager.py", "src/xfce/ThemeManager.py", "src/xfce/ScaleManager.py"]),
+    ("/usr/share/pardus/pardus-welcome/src/gnome", ["src/gnome/WallpaperManager.py", "src/gnome/ThemeManager.py", "src/gnome/ScaleManager.py"]),
+    ("/usr/share/pardus/pardus-welcome/ui", ["ui/MainWindow.glade"]),
     ("/usr/bin/", ["pardus-welcome"])
 ]
 
