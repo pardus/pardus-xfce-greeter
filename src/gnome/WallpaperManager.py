399a315ba5f0589b1e5374826fb1d80218c6e53c
Emin Fedar <eminfedar@gmail.com>
2021-08-16 18:33:19 +0300

    WallpaperManager only detect current resolution svg on desktop-base

--- src/gnome/WallpaperManager.py
+++ src/gnome/WallpaperManager.py
@@ -1,14 +1,22 @@
 import os, subprocess
 
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import GLib, Gio, Gtk
 
-folders = ["/usr/share/backgrounds/", "/usr/share/desktop-base/active-theme/wallpaper/contents/images/"]
+folders = ["/usr/share/backgrounds/"]
+desktopBaseFolder = "/usr/share/desktop-base/active-theme/wallpaper/contents/images/"
 prefixs = ["jpg","png","bmp","jpeg","svg"]
 
 def getWallpaperList():
+    currentResolution = getResolution()
+    desktopBaseResolutions = []
     pictures = []
+
+    # Get all resolutions from desktop-base and select Current system resolution if exists
+    desktopBaseFiles = os.walk(desktopBaseFolder)
+    for (_, _, files) in desktopBaseFiles:
+        for file in files:
+            desktopBaseResolutions.append(f"{file}")
+    
+    # Add other wallpapers to list
     for path in folders:
         paths = os.walk( path )
         for (dirpath, _, files) in paths:
@@ -17,6 +25,12 @@ def getWallpaperList():
                     if file[-3:] == prefix:
                         pictures.append(f"{dirpath}/{file}")
                         break
+    
+    # Add desktop-base wallpaper to list
+    if f"{currentResolution}.svg" in desktopBaseResolutions:
+        pictures.append(f"{desktopBaseFolder}{currentResolution}.svg")
+    else:
+        pictures = pictures + desktopBaseResolutions
     
     return pictures
 
@@ -28,3 +42,8 @@ def setWallpaper(wallpaper):
         "picture-uri",
         f"file://{wallpaper}"
     ])
+
+
+def getResolution():
+    output = subprocess.check_output("xrandr | grep '*'", shell=True).decode("utf-8")
+    return output.strip().split(" ")[0]
--- src/xfce/WallpaperManager.py
+++ src/xfce/WallpaperManager.py
@@ -1,14 +1,22 @@
 import os, subprocess
 
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import GLib, Gio, Gtk
 
-folders = ["/usr/share/backgrounds/", "/usr/share/desktop-base/active-theme/wallpaper/contents/images/"]
+folders = ["/usr/share/backgrounds/"]
+desktopBaseFolder = "/usr/share/desktop-base/active-theme/wallpaper/contents/images/"
 prefixs = ["jpg","png","bmp","jpeg","svg"]
 
 def getWallpaperList():
+    currentResolution = getResolution()
+    desktopBaseResolutions = []
     pictures = []
+
+    # Get all resolutions from desktop-base and select Current system resolution if exists
+    desktopBaseFiles = os.walk(desktopBaseFolder)
+    for (_, _, files) in desktopBaseFiles:
+        for file in files:
+            desktopBaseResolutions.append(f"{file}")
+    
+    # Add other wallpapers to list
     for path in folders:
         paths = os.walk( path )
         for (dirpath, _, files) in paths:
@@ -18,10 +26,24 @@ def getWallpaperList():
                         pictures.append(f"{dirpath}/{file}")
                         break
     
+    # Add desktop-base wallpaper to list
+    if f"{currentResolution}.svg" in desktopBaseResolutions:
+        pictures.append(f"{desktopBaseFolder}{currentResolution}.svg")
+    else:
+        pictures = pictures + desktopBaseResolutions
+    
     return pictures
 
 def setWallpaper(wallpaper):
     subprocess.call([
-        "/bin/sh", "-c",
-        f"xfconf-query -c xfce4-desktop -l | grep last-image | while read path; do xfconf-query -c xfce4-desktop -p $path -s {wallpaper}; done"
-    ])       
+        "gsettings",
+        "set",
+        "org.gnome.desktop.background",
+        "picture-uri",
+        f"file://{wallpaper}"
+    ])
+
+
+def getResolution():
+    output = subprocess.check_output("xrandr | grep '*'", shell=True).decode("utf-8")
+    return output.strip().split(" ")[0]
