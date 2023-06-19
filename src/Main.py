5fb1f57a40c348f8e6ab0b8dda80f07cad550a9c
Fatih Altun <fatih.altun@pardus.org.tr>
2023-06-19 18:42:15 +0300

    Add gio application flag

--- src/Main.py
+++ src/Main.py
@@ -5,14 +5,15 @@ import sys
 import gi
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gio, Gtk
 
 from MainWindow import MainWindow
 
 
 class Application(Gtk.Application):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, application_id="tr.org.pardus.xfce-greeter", **kwargs)
+        super().__init__(*args, application_id="tr.org.pardus.xfce-greeter", flags=Gio.ApplicationFlags.NON_UNIQUE,
+                         **kwargs)
         self.window = None
 
     def do_activate(self):
