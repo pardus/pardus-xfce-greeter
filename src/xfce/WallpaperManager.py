81d073fdda437e02fc47f435d6746cc134c7bb00
Fatih Altun <fatih.altun@pardus.org.tr>
2023-08-25 03:37:10 +0300

    Update get wallpaper func

--- src/xfce/WallpaperManager.py
+++ src/xfce/WallpaperManager.py
@@ -3,47 +3,19 @@
 import os
 import subprocess
 
-folders = ["/usr/share/backgrounds/"]
-desktopBaseFolder = "/usr/share/desktop-base/active-theme/wallpaper/contents/images/"
-prefixs = ["jpg", "png", "bmp", "jpeg", "svg"]
+wallpaper_dir = "/usr/share/backgrounds/"
 
 
 def getWallpaperList():
-    currentResolution = getResolution()
-    currentResolutionTuple = (int(currentResolution.split("x")[0]), int(currentResolution.split("x")[1]))
-    nearestResolution = (1, 1)
-    nearestResolutionFile = ""
-    pictures = []
+    wallpapers = []
+    # Add wallpapers to list
+    for root, dirs, files in os.walk(wallpaper_dir):
+        dirs.clear()
+        for file_name in files:
+            path = os.path.join(root, file_name)
+            wallpapers.append(f"{path}")
 
-    # Get all resolutions from desktop-base and select Current system resolution if exists
-    desktopBaseFiles = os.walk(desktopBaseFolder)
-    for (_, _, files) in desktopBaseFiles:
-        for file in files:
-            fileResolution = (int(file[:-4].split("x")[0]), int(file[:-4].split("x")[1]))
-            # print(f"Current: {currentResolutionTuple}, File: {fileResolution}, Fark:{abs(currentResolutionTuple[0] - fileResolution[0])}, {abs(currentResolutionTuple[1] - fileResolution[1])}")
-            if abs(currentResolutionTuple[0] - fileResolution[0]) <= abs(
-                    currentResolutionTuple[0] - nearestResolution[0]):
-                if abs(currentResolutionTuple[1] - fileResolution[1]) <= abs(
-                        currentResolutionTuple[1] - nearestResolution[1]):
-                    # print(f"en uygun bulundu: {fileResolution}, Fark:{abs(currentResolutionTuple[0] - nearestResolution[0])}, {abs(currentResolutionTuple[1] - nearestResolution[1])}")
-                    nearestResolution = fileResolution
-                    nearestResolutionFile = file
-
-    # Add desktop-base wallpaper to list
-    pictures.append(f"{desktopBaseFolder}{nearestResolutionFile}")
-
-    # Add other wallpapers to list
-    for path in folders:
-        paths = os.walk(path)
-        for (dirpath, _, files) in paths:
-            if dirpath != "/usr/share/backgrounds/xfce":
-                for file in files:
-                    for prefix in prefixs:
-                        if file[-3:] == prefix:
-                            pictures.append(f"{dirpath}/{file}")
-                            break
-
-    return pictures
+    return wallpapers
 
 
 def setWallpaper(wallpaper):
